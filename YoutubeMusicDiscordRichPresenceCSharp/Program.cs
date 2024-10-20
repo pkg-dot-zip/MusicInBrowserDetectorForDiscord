75fd036b424de82476523ad3cc47fd7301a7db23
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-20 16:41:57 +0200

    Now auto update presence

--- YoutubeMusicDiscordRichPresenceCSharp/Program.cs
+++ YoutubeMusicDiscordRichPresenceCSharp/Program.cs
@@ -6,12 +6,18 @@ namespace YoutubeMusicDiscordRichPresenceCSharp;
 internal class Program
 {
     private static readonly IBrowser BrowserHandler = new ChromeHandler();
+    private static bool _shouldQuit = false;
 
     public static void Main(string[] args)
     {
         Run(BrowserHandler);
     }
 
+    /// <summary>
+    /// Run the program.
+    /// </summary>
+    /// <param name="browserHandler">What browser should be used.</param>
+    /// <param name="refreshInterval">How often rpc should be updated in ms.</param>
     private static void Run(IBrowser browserHandler, int refreshInterval = 20000)
     {
         Initialize();
@@ -20,7 +26,26 @@ internal class Program
 
         Thread.Sleep(2000); // Wait 2 sec.
 
-        while (true)
+        Thread presenceThread = new Thread(() => UpdatePresence(browserHandler, refreshInterval))
+        {
+            Name = "ytmrpcupdatethread",
+            IsBackground = true,
+        };
+        presenceThread.Start();
+
+        while (!_shouldQuit)
+        {
+            var input = Console.ReadLine()?.ToLower().Trim();
+            _shouldQuit = input is "quit" or "q";
+        }
+
+        presenceThread.Join();
+        Deinitialize();
+    }
+
+    private static void UpdatePresence(IBrowser browserHandler, int refreshInterval)
+    {
+        while (!_shouldQuit)
         {
             var playingInfo = SongRetriever.FromBrowser(browserHandler.GetDriver());
 
@@ -35,18 +60,21 @@ internal class Program
 
             Thread.Sleep(refreshInterval);
         }
-
-        Deinitialize();
     }
 
     private static void Initialize()
     {
+        Console.WriteLine("Initializing...");
         RpcHandler.Initialize();
+        Console.WriteLine("Initialized.");
+        Console.WriteLine("\n\n\tWrite quit (or q) to exit.\t\t");
     }
 
     private static void Deinitialize()
     {
+        Console.WriteLine("Deinitializing...");
         RpcHandler.Deinitialize();
         BrowserHandler.Close();
+        Console.WriteLine("Deinitialized.");
     }
 }
