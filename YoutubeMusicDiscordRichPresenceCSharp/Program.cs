0d7a49987baba70e88fd17ac0035b62b8e62c193
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-20 10:16:15 +0200

    Add project files.

---
+++ YoutubeMusicDiscordRichPresenceCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using DiscordRPC;
+using YoutubeMusicDiscordRichPresenceCSharp.Rpc;
+
+namespace YoutubeMusicDiscordRichPresenceCSharp;
+
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        RpcHandler.Initialize();
+
+        RpcHandler.SetPresence(new RichPresence()
+        {
+            Details = "Example Project",
+            State = "csharp example",
+            Assets = new Assets()
+            {
+                LargeImageKey = "image_large",
+                LargeImageText = "Lachee's Discord IPC Library",
+                SmallImageKey = "image_small"
+            }
+        });
+
+        Console.ReadKey();
+
+        RpcHandler.Deinitialize();
+    }
+}
