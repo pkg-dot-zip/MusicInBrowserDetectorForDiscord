76ecd5cff96a9bd6930ac29189f87ee385b84256
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-20 16:21:34 +0200

    Refresh interval now a param

--- YoutubeMusicDiscordRichPresenceCSharp/Program.cs
+++ YoutubeMusicDiscordRichPresenceCSharp/Program.cs
@@ -12,18 +12,17 @@ internal class Program
         Run(BrowserHandler);
     }
 
-    private static void Run(IBrowser browserHandler)
+    private static void Run(IBrowser browserHandler, int refreshInterval = 20000)
     {
         Initialize();
 
         if (!browserHandler.IsRunning()) browserHandler.OpenWindow();
 
-        Thread.Sleep(5000);
+        Thread.Sleep(2000); // Wait 2 sec.
 
         while (true)
         {
             var playingInfo = SongRetriever.FromBrowser(browserHandler.GetDriver());
-
 
             if (playingInfo is not null)
             {
@@ -34,8 +33,7 @@ internal class Program
                 Console.WriteLine("Couldn't find song, thus no rpc set.");
             }
 
-            Thread.Sleep(5000);
-            if (Console.ReadLine() == "c") break;
+            Thread.Sleep(refreshInterval);
         }
 
         Deinitialize();
