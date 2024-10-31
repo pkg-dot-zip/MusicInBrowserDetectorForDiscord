630ececf99f30d89a47a3adce18c26c5db5ac7fd
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-10-31 16:12:18 +0100

    Now look for open processes to select BrowserHandler

--- YoutubeMusicDiscordRichPresenceCSharp/Program.cs
+++ YoutubeMusicDiscordRichPresenceCSharp/Program.cs
@@ -1,11 +1,12 @@
-﻿using YoutubeMusicDiscordRichPresenceCSharp.Browser;
+﻿using System.Diagnostics;
+using YoutubeMusicDiscordRichPresenceCSharp.Browser;
 using YtmRcpLib.Rpc;
 
 namespace YoutubeMusicDiscordRichPresenceCSharp;
 
 internal class Program
 {
-    private static readonly IBrowser BrowserHandler = new ChromeHandler();
+    private static readonly IBrowser BrowserHandler = GetBrowserHandler();
     private static bool _shouldQuit = false;
 
     public static void Main(string[] args)
@@ -68,6 +69,28 @@ internal class Program
 
             Thread.Sleep(refreshInterval);
         }
+    }
+
+    private static IBrowser GetBrowserHandler()
+    {
+        var processlist = Process.GetProcesses();
+
+        Console.Out.WriteLine("Looking for Browser:");
+
+        foreach (var process in processlist)
+        {
+            if (string.IsNullOrEmpty(process.MainWindowTitle)) continue; // Needs to have an open window.
+
+            Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+
+            if (process.ProcessName == "chrome")
+            {
+                Console.Out.WriteLine("Found Chrome!");
+                return new ChromeHandler();
+            }
+        }
+
+        throw new InvalidOperationException("Couldn't find an opened browser! Check our github repository for information on what browsers are supported.");
     }
 
     private static void Initialize()
