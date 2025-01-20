863535178799bf2b2056b1c9f9838424187f1fa2
pkg-dot-zip <simon@pkg-dot-zip.com>
2025-01-20 21:03:28 +0100

    Now use Timer instead of manual threading

--- YoutubeMusicDiscordRichPresenceCSharp/MusicBrowserDetectorProgram.cs
+++ YoutubeMusicDiscordRichPresenceCSharp/MusicBrowserDetectorProgram.cs
@@ -1,96 +1,78 @@
-﻿using OpenQA.Selenium;
+﻿using System.Timers;
 using YoutubeMusicDiscordRichPresenceCSharp.Browser;
 using YoutubeMusicDiscordRichPresenceCSharp.Browser.Retriever;
 using YoutubeMusicDiscordRichPresenceCSharp.Rpc;
+using Timer = System.Timers.Timer;
 
 namespace YoutubeMusicDiscordRichPresenceCSharp;
 
 public class MusicBrowserDetectorProgram(IBrowserRetriever browserRetriever)
 {
-    private static bool _shouldQuit = false;
+    private bool _shouldQuit = false;
+    private readonly IBrowser _browserHandler = browserRetriever.GetBrowserHandler();
+    private readonly PresenceUpdater _presenceUpdater = new PresenceUpdater();
+    private readonly Timer _timer = new Timer
+    {
+        AutoReset = true,
+        Enabled = false,
+        Interval = 5000,
+    };
 
     public void Run()
-    {
-        Run(browserRetriever.GetBrowserHandler());
-    }
-
-    /// <summary>
-    /// Run the program.
-    /// </summary>
-    /// <param name="browserHandler">What browser should be used.</param>
-    /// <param name="refreshInterval">How often rpc should be updated in ms.</param>
-    private void Run(IBrowser browserHandler, int refreshInterval = 20000)
     {
         Initialize();
 
         Thread.Sleep(2000); // Wait 2 sec.
 
-        Thread presenceThread = new Thread(() => UpdatePresence(browserHandler, refreshInterval))
-        {
-            Name = "musicinbrowserrpcupdatethread",
-            IsBackground = true,
-        };
-        presenceThread.Start();
-
+        StartTimer();
         while (!_shouldQuit)
         {
             var input = Console.ReadLine()?.ToLower().Trim();
             _shouldQuit = input is "quit" or "q";
+            if (_shouldQuit) StopTimer();
         }
 
-        presenceThread.Join();
-        DeInitialize(browserHandler);
+        DeInitialize();
     }
 
-    private static void UpdatePresence(IBrowser browserHandler, int refreshInterval)
+    private void StartTimer()
     {
-        browserHandler.GetDriver(); // Call this so retriever gets initialized. Great system cough cough :/
-        var retriever = browserHandler.GetRetriever();
-        if (retriever is null)
-        {
-            Console.WriteLine("Couldn't find support for this service. Not updating.");
-            return;
-        }
-
-        while (!_shouldQuit)
-        {
-            try
-            {
-                var playingInfo = retriever.FromBrowser(browserHandler);
-
-                if (playingInfo is not null)
-                {
-                    RpcHandler.SetPresence(SongPresenceHandler.GetSongPresence(retriever, playingInfo));
-                }
-                else
-                {
-                    Console.WriteLine("Couldn't find song, thus no rpc set.");
-                }
-
-                Thread.Sleep(refreshInterval);
-            }
-            catch (NoSuchWindowException ex)
-            {
-                Console.Out.WriteLine("Looks like the browser window we were hooked too was closed. Disconnected.");
-                _shouldQuit = true;
-                DeInitialize(browserHandler);
-            }
-        }
+        Console.WriteLine("Starting Timer...");
+        _timer.Start();
+        Console.WriteLine("Started Timer.");
     }
 
-    private static void Initialize()
+    private void StopTimer()
+    {
+        Console.WriteLine("Stopping Timer...");
+        _timer.Stop();
+        Console.WriteLine("Stopped Timer.");
+    }
+
+    private void OnTimerElapsed(object? sender, ElapsedEventArgs e)
+    {
+        // TODO: Only do this once.
+        _browserHandler.GetDriver(); // Call this so retriever gets initialized. Great system cough cough :/
+
+        _presenceUpdater.UpdatePresence(_browserHandler);
+    }
+
+    private void Initialize()
     {
         Console.WriteLine("Initializing...");
         RpcHandler.Initialize();
         Console.WriteLine("Initialized.");
         Console.WriteLine("\n\n\tWrite quit (or q) to exit.\t\t");
+
+        _timer.Elapsed += OnTimerElapsed;
     }
 
-    private static void DeInitialize(IBrowser browserHandler)
+    private void DeInitialize()
     {
+        _timer.Elapsed -= OnTimerElapsed;
         Console.WriteLine("Deinitializing...");
         RpcHandler.Deinitialize();
-        browserHandler.Close();
+        _browserHandler.Close();
         Console.WriteLine("Deinitialized.");
         Environment.Exit(0);
     }
---
+++ YoutubeMusicDiscordRichPresenceCSharp/Rpc/PresenceUpdater.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using YoutubeMusicDiscordRichPresenceCSharp.Browser;
+
+namespace YoutubeMusicDiscordRichPresenceCSharp.Rpc;
+
+public class PresenceUpdater
+{
+    public bool UpdatePresence(IBrowser browserHandler)
+    {
+        try
+        {
+            var retriever = browserHandler.GetRetriever();
+            if (retriever is null) return false;
+
+
+            var playingInfo = retriever.FromBrowser(browserHandler);
+
+            if (playingInfo is not null)
+            {
+                RpcHandler.SetPresence(SongPresenceHandler.GetSongPresence(retriever, playingInfo));
+            }
+            else
+            {
+                Console.WriteLine("Couldn't find song, thus no rpc set.");
+            }
+
+            return true;
+        }
+        catch (NoSuchWindowException ex)
+        {
+            Console.Out.WriteLine("Looks like the browser window we were hooked too was closed. Disconnected.");
+            return false;
+        }
+    }
+}
